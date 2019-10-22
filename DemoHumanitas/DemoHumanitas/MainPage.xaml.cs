eb180c6f19bf8669d594d6a071e5c1f20fe0cba7
Juliano Custodio <juliano.custodio@hotmail.com.br>
2019-10-22 20:52:23 -0300

    Adiciona projeto feito no workshop

---
+++ DemoHumanitas/DemoHumanitas/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoHumanitas
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Somar(object sender, System.EventArgs e)
+        {
+            
+            int primeiro = int.Parse(NumeroUm.Text);
+            int segundo = int.Parse(NumeroDois.Text);
+            int resultado = primeiro + segundo;
+
+            LabelResultado.Text = "Total: "+ resultado.ToString();
+        }
+    }
+}
